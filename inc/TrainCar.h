2ea7a0dabebcb0726c42766f011d512f2f6dc44a
Brendan Czarnecki <czarnecki.brendan@gmail.com>
2020-06-16 23:02:21 -0400

    Added ErrorStatus enum and made it public

--- inc/TrainCar.h
+++ inc/TrainCar.h
@@ -9,10 +9,17 @@ private:
     TrainCar* next_car_;
     BrakeStatus brake_status_;
 public:
+    enum class ErrorStatus
+    {
+        kNoError = 0,
+        kBrakeEngageError,
+        kBrakeDisengageError,
+        kAttachCarError
+    };
     TrainCar();
-    int ActivateBrake();
-    int DeactivateBrake();
-    int AttachCar(TrainCar*);
+    ErrorStatus ActivateBrake();
+    ErrorStatus DeactivateBrake();
+    ErrorStatus AttachCar(TrainCar*);
     virtual ~TrainCar();
 };
 
